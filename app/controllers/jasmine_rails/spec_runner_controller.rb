101c28a8e5b83caf827c4bf3f8070ece4c2716a7
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2013-05-06 13:40:07 +0300

    Skip cancan's authorization check for SpecRunnerController

--- app/controllers/jasmine_rails/spec_runner_controller.rb
+++ app/controllers/jasmine_rails/spec_runner_controller.rb
@@ -1,5 +1,7 @@
 module JasmineRails
   class SpecRunnerController < ApplicationController
+    skip_authorization_check if defined?(CanCan)
+
     def index
       JasmineRails.reload_jasmine_config
     end
